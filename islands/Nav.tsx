9bd606143789a115c502827dd2b25462f353b9ed
‘uiuxworks’ <sabersworks@gmail.com>
2022-09-17 15:07:19 +0700

    push

---
+++ islands/Nav.tsx
@@ -0,0 +1,20 @@
+export default function Nav(){
+    return (
+    <div class="fixed max-w-[400px] mx-auto inset-x-0 bottom-0 py-4 bg-white">
+    <div class="flex justify-between items-center space-x-4 w-full px-8">
+    <div class="text-center text-[#1A2637]">
+        <div class="font-bold text-md">Home</div>
+    </div>
+    <div class="text-center text-[#1A2637]">
+        <div class="font-bold text-md">Date</div>
+    </div>
+    <div class="text-center text-[#1A2637]">
+        <div class="font-bold text-md">RSVP</div>
+    </div>
+    <div class="text-center text-[#1A2637]">
+        <div class="font-bold text-md">Gift</div>
+    </div>
+    </div>
+    </div>
+    );
+}
